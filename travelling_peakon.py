0719a63475c12b6cc1ff9f49b88fd1a2675f62f4
Dominic Whitaker <dominic.whitaker20@imperial.ac.uk>
2022-05-30 13:39:51 +0100

    simple 2 peak example, properly commented code

--- travelling_peakon.py
+++ travelling_peakon.py
@@ -1,24 +1,26 @@
 import numpy as np
-from numpy.random import random
 import matplotlib.pyplot as plt
 from time import perf_counter
 
-t0 = perf_counter()
+# t0 = perf_counter()
 
-peakon_number = 5 # number of peaks
+### NUMERICAL PROPERTIES
+
 sample_number = 10000 # number of x values to sample u_func
-x_range = np.linspace(0, 100, sample_number) # x values u_func is sampled out
+x_range = np.linspace(-100, 100, sample_number) # x values u_func is sampled out
 t_max = 200 # max t value to sample u_func
 delta_t = 0.025 # timestep to advance u_func by each iteration
-m_range = 0.5*random(size=peakon_number) + 0.5 # array of peakon heights
-np.sort(m_range)
-c_range = 10*random(size=peakon_number) + 5
-np.sort(c_range)
-offset = 20*random(size=peakon_number) - 20
-np.sort(offset)[::-1]
+
+### PEAKON PROPERTIES
+
+peakon_number = 2 # number of peaks
+c_range = np.array([0, 5]) # speed of each peak
+m_range = np.array([1, 1]) # height of each peak
+offset = np.array([0, -50]) # peak starting location
 
 u_func = lambda x, t: np.sum(m_range[:,None] * np.exp(-np.abs(x[None,:]-c_range[:,None]*t - offset[:,None])),axis=0)
 
+### PLOTTING ZONE
 
 f, ax = plt.subplots(1)
 
@@ -31,8 +33,8 @@ while t <= t_max:
     plt.cla()
     u_vec = u_func(x_range, t)
     ax.plot(x_range, u_vec)
-    plt.ylim([0,2])
-    t1 = perf_counter()
-    plt.title(f"{(t/delta_t)/(t1-t0)} fps")
+    plt.ylim([0,2.5]) # set slightly higher than largest u value
+    # t1 = perf_counter()
+    # plt.title(f"{(t/delta_t)/(t1-t0)} fps")
     plt.pause(0.000000000000000000000000000000001)
     t += delta_t
