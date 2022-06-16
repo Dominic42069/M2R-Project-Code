d588868ae16c2a562da03419968574caf31d3f76
Dominic Whitaker <dominic.whitaker20@imperial.ac.uk>
2022-06-16 17:00:54 +0100

    colour gradient + saving

--- travelling_peakon.py
+++ travelling_peakon.py
@@ -5,17 +5,17 @@ import sys
 ### NUMERICAL PROPERTIES
 
 sample_number = 10000 # number of x values to sample u_func
-x_range = np.linspace(0, 200, sample_number) # x values u_func is sampled out
-t_max = 200 # max t value to sample u_func
-delta_t = 2 # timestep to advance u_func by each iteration
+endpoint = 25
+x_range = np.linspace(0, endpoint, sample_number) # x values u_func is sampled out
+t_max = 2.5 # max t value to sample u_func
+delta_t = 0.1 # timestep to advance u_func by each iteration
 
 ### PEAKON PROPERTIES
 
-peakon_number = 2 # number of peaks
-c_range = 5*np.array([1, 3]) # speed of each peak
-offset = np.array([5, 5]) # peak starting location.
+c_range = 5*np.array([3, 6]) # speed of each peak
+offset = np.array([5,5]) # peak starting location.
 
-u_func = lambda x, t: np.sum(1/25 * c_range[:,None] * np.exp(-np.abs((x[None,:]-c_range[:,None]*t) % 200 - offset[:,None])),axis=0)
+u_func = lambda x, t: np.sum(1/25 * c_range[:,None] * np.exp(-np.abs((x[None,:]-c_range[:,None]*t) % endpoint - offset[:,None])),axis=0)
 
 ### PLOTTING ZONE
 
@@ -23,11 +23,21 @@ f, ax = plt.subplots(1)
 
 t=0
 
+colours = np.linspace(0, 1, int(t_max/delta_t),endpoint=False)
+
+hex = [hex(i)[-2:] for i in range(256)]
+
+hex_colours = [f'#{r}00{b}' for r, b in zip(hex, hex[::-1])]
+reduced_hex_colours = [hex_colours[i] for i in range(256) if i%8==0]
+reduced_hex_colours = [col.replace('x', '0') for col in reduced_hex_colours]
+
 while t <= t_max:
-    u_vec = u_func(x_range, t) + 0.1*t
-    ax.plot(x_range, u_vec)
-    plt.ylim([0,10]) # set slightly higher than largest u value
+    u_vec = u_func(x_range, t) + t/delta_t
+    ax.plot(x_range, u_vec, color=reduced_hex_colours[int(t/delta_t)])
+    plt.yticks([])
+    plt.ylim([0,25]) # set slightly higher than largest u value
     plt.pause(0.000000000000000000000000000000001)
     t += delta_t
 
+plt.savefig("test_save")
 plt.show()
